d4266b8edee29333a2dbe5b35bfa7752b0593533
Samuel Ramos <samramox@gmail.com>
2018-06-23 18:49:43 -0300

    Fixed shelf title

--- src/components/BookShelfRow/index.js
+++ src/components/BookShelfRow/index.js
@@ -1,12 +1,25 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import Book from '../Book';
 
-class BookShelfRow extends PureComponent {
+class BookShelfRow extends Component {
+  title = shelf => {
+    switch (shelf) {
+      case 'currentlyReading':
+        return 'Currently Reading';
+      case 'wantToRead':
+        return 'Want To Read';
+      case 'read':
+        return 'Read';
+      default:
+        return;
+    }
+  };
+
   renderBooks = (shelf, books, handleChange) => {
     if (shelf) {
       return (
         <div className="bookshelf">
-          <h2 className="bookshelf-title">{shelf}</h2>
+          <h2 className="bookshelf-title">{this.title(shelf)}</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
               {books.map(book => (
